1da03d76fdefdd598bc201f46b6c287d7844bcd7
Clifford-coder <cliffowusu2727@gmail.com>
2023-09-26 14:33:20 +0000

    feat: Added prettier setup for code formatting

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 function App() {
-  return (
-    <div className="min-h-screen flex justify-center items-center">
-      <h1 className="text-3xl font-bold text-blue-600">Hello Prom AI</h1>
-    </div>
-  );
+	return (
+		<div className="min-h-screen flex justify-center items-center">
+			<h1 className="text-3xl font-bold text-blue-600">Hello Prom AI</h1>
+		</div>
+	);
 }
 
 export default App;
