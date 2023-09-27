d10e3114420161718edaf2aadd9b5f06cbb789d9
Tijani Femi <olutimedia@gmail.com>
2023-09-27 11:00:58 +0100

    fix type error

--- src/__text__/App.spec.tsx
+++ src/__text__/App.spec.tsx
@@ -1,11 +1,11 @@
-// import { render } from '@testing-library/react';
-// import App from '../App';
+import { render } from '@testing-library/react';
+import App from '../App';
 
 test('demo', () => {
 	expect(true).toBe(true);
 });
 
-// test('Renders the main page', () => {
-// 	render(<App />);
-// 	expect(true).toBeTruthy();
-// });
+test('Renders the main page', () => {
+	render(<App />);
+	expect(true).toBeTruthy();
+});
